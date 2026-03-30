16075127c186a3a00512cae46e06febbd68f9ff3
Furkan Özay <furkanczay@gmail.com>
2026-03-30 08:25:04 +0300

    Refactor RootLayout component: Update logo with Image component, improve formatting, and ensure consistent code style

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Lexend } from "next/font/google"
+import { Lexend } from "next/font/google";
 import "@/app/globals.css";
 import { Toaster } from "sonner";
 import Providers from "@/providers";
@@ -8,52 +8,53 @@ import LocaleSwitcher from "@/components/language-switcher";
 import ThemeChanger from "@/components/modetoggle";
 import Footer from "@/components/footer";
 import Link from "next/link";
+import Image from "next/image";
 
 const font = Lexend({
   subsets: ["latin"],
-})
-
+});
 
 export const metadata: Metadata = {
   title: "Czaylabs | Random Password Generator",
-  description: "Create strong and secure passwords to keep your account safe online.",
+  description:
+    "Create strong and secure passwords to keep your account safe online.",
 };
 
 export default async function RootLayout({
   children,
-  params
+  params,
 }: Readonly<{
-  children: React.ReactNode,
-  params: Promise<{ lang: Locale }>
+  children: React.ReactNode;
+  params: Promise<{ lang: Locale }>;
 }>) {
-
   const { lang } = await params;
   return (
     <html lang={lang} suppressHydrationWarning>
-      <body
-        className={`${font.className} antialiased`}
-      >
+      <body className={`${font.className} antialiased`}>
         <Providers>
-            <div className="max-w-(--breakpoint-xl) mx-auto">
-              <div className="flex justify-between items-center px-5 md:px-20 my-10">
-                  <Link href="/" className="inline-flex flex-col gap-1 items-center sm:flex-row sm:items-end">
-                      <span className="text-4xl font-bold">CzayLabs</span> 
-                      <div className="text-md">
-                          <span className="text-yellow-400 text-md">P</span>
-                          <span className="text-xs">assword</span> 
-                          <span className="text-yellow-400">G</span>
-                          <span className="text-xs">enerator</span>
-                      </div>
-                  </Link>
-                  <div className="inline-flex gap-5 items-center">
-                      <LocaleSwitcher locale={lang} />
-                      <ThemeChanger />
-                  </div>
+          <div className="max-w-(--breakpoint-xl) mx-auto">
+            <div className="flex justify-between items-center px-5 md:px-20 my-10">
+              <Link
+                href="/"
+                className="inline-flex flex-col gap-1 items-center sm:flex-row sm:items-end"
+              >
+                <Image
+                  src="/pwg-czaylabs-logo.svg"
+                  alt="PWG Czaylabs Logo"
+                  width={100}
+                  height={100}
+                  className="h-90 w-auto rounded-full dark:invert"
+                />
+              </Link>
+              <div className="inline-flex gap-5 items-center">
+                <LocaleSwitcher locale={lang} />
+                <ThemeChanger />
               </div>
-              {children}
-              <Footer />
             </div>
-            <Toaster position="top-right" />
+            {children}
+            <Footer />
+          </div>
+          <Toaster position="top-right" />
         </Providers>
       </body>
     </html>
